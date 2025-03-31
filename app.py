b4295e8f1238ec4380d94207c9400c50b3605a39
giang-ngo <giangngo.py@gmail.com>
2025-03-31 22:46:41 +0700

    Add basic store and item API endpoints

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": 'My store',
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            },
+            {
+                "name": "Book",
+                "price": 18.00
+            },
+            {
+                "name": "Cream",
+                "price": 25.99
+            },
+            {
+                "name": "Head phone",
+                "price": 45.99
+            }
+        ]
+    }
+]
+
+
+@app.post('/store')
+def create_store():
+    data = request.get_json()
+    new_store = {'name': data['name'], 'items': []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+@app.post('/store/<string:name>/item')
+def create_item(name):
+    data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {'name': data['name'], 'price': data['price']}
+            store['items'].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store['items']}
+    return {"message": "Store not found"}, 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
